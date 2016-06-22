0a2e02e3b49723e7856d654b825b37295d339c20
Caleb Jones <self@calebjones.net>
2016-06-22 12:03:05 -0400

    Refactor routes into another file

--- Main.go
+++ Main.go
@@ -3,16 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-
-	router.HandleFunc("/", Index)
-	router.HandleFunc("/entries", EntryIndex)
-	router.HandleFunc("/entries/{entryId}", EntryShow)
+	router := NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
